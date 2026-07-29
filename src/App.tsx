a3166bb6ae4a68c81654d39ba93c4df7581b289d
Joss-gif321 <parakteno.321@gmail.com>
2026-07-29 13:34:00 +0000

    tamanhos imagem

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <img
-        src="https://picsum.photos/400/300"
-        alt="Imagem responsiva"
-        className="App-image"
-      />
-    </div>
+    <>
+      <picture>
+      <source media="(min-width: 1000px)" srcSet="https://picsum.photos/id/5/1200/1200" />
+      <source media="(min-width: 750px)" srcSet="https://picsum.photos/id/5/1000/1000" />
+      <source media="(min-width: 500px)" srcSet="https://picsum.photos/id/5/750/750" />
+      <img src="https://picsum.photos/id/5/500/500" />
+      </picture>
+
+    </>
   )
 }
 
